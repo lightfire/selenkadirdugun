d4e793888f6e5a76223981122100da6cc69b4df3
root <root@s1.asurekazani.com>
2016-09-05 13:25:55 -0400

    init gitlab

---
+++ public/gitlab.php
@@ -0,0 +1,17 @@
+<?php
+
+// Get Branch Name.
+$branch = isset($_GET['branch']) ? $_GET['branch'] : basename(realpath(__DIR__.'/..'));
+
+// This Branch Name is Valid?
+if ( !is_null($branch) )
+{
+    // Notifications.
+    $messages = array();
+
+    // Pull Last Changes.
+    $messages[] = shell_exec("git pull origin {$branch}");
+
+    // Write To All Notifications.
+    foreach($messages as $message) echo nl2br($message);
+}
